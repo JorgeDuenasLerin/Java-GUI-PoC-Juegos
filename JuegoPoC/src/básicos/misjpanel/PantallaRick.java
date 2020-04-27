1393178285539bf662404ef3f5600cd449379576
JorgeDuenasLerin <jorge.duenas.lerin@gmail.com>
2020-04-27 17:48:08 +0200

    Ejemplo de commit en clase

--- JuegoPoC/src/básicos/misjpanel/PantallaRick.java
+++ JuegoPoC/src/básicos/misjpanel/PantallaRick.java
@@ -19,7 +19,7 @@ public class PantallaRick extends JPanel{
 		etiqueta1 = new JLabel(new ImageIcon(ConImagen.class.getResource("/recursos/rick_morty.png")));
 		etiqueta1.setBorder(BorderFactory.createLineBorder(Color.BLUE));
 		
-		etiqueta2 = new JLabel("Esto es el label de Rick");
+		etiqueta2 = new JLabel("Esto es el label de Rick!!");
 		this.add(etiqueta1);
 	}
 	
